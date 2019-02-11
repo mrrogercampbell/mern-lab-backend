d8d254eef34adc05c24a2ef14d19f5b8d6e7f056
Roger Campbell II <rc1336@gmail.com>
2019-02-11 16:07:10 -0500

    added route const and app.use routes to index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(parser.json())
 app.use(cors())
 app.use(express.static('client/build'))
 
+const routes = require('./route/animalRoute')
 
+app.use('/', routes)
 
 
 app.listen(3001, () => {
